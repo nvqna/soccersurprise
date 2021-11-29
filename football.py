2e530ffa28765ae2b79cbb3cae6ea45994825b3a
Aidan <github.com@nvqna.com>
2021-11-28 20:51:00 -0500

    Adds functionality to sort by number of goals descending, without displaying goal count.

--- football.py
+++ football.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
+from collections import defaultdict
 from datetime import date,timedelta
 import requests
+import re
 
 # returns a list of all saturday dates for a year
 def allSaturdays(year):
@@ -19,7 +21,11 @@ def allSundays(year):
         yield d
         d += timedelta(days = 7)
 
-
+def getExtraTime(event):
+    totalTime = event['status']['displayClock']
+    regex = r".*\+(\d*)\'"
+    matches = re.search(regex,totalTime)
+    return matches.group(1)
 
 
 api = "http://site.api.espn.com/apis/site/v2/sports/soccer/eng.1/scoreboard?lang=en&region=gb&calendartype=whitelist&limit=100&league=eng.1&dates="
@@ -29,11 +35,27 @@ url = api + date
 
 response = requests.get(url)
 
+# dict with n goals as key, array of matches as value
+goals_dict = defaultdict(list)
+"""
+{
+    "0":["Chelsea vs Liverpool", "Watford vs Arsenal"]
+    ...
+    "5":["Manchester City vs Manchester United"]
+}
+
+"""
+
 for event in response.json()['events']:
     team0Name = event['competitions'][0]['competitors'][0]['team']['name']
     team1Name = event['competitions'][0]['competitors'][1]['team']['name']
+    title = team0Name + " vs. " + team1Name
     team0Score = event['competitions'][0]['competitors'][0]['score']
     team1Score = event['competitions'][0]['competitors'][1]['score'] 
     totalGoals = int(team0Score) + int(team1Score)
-    print(team0Name + " vs " + team1Name + " (" + str(totalGoals) + ")")
+    extraTime = getExtraTime(event)
+    goals_dict[totalGoals].append(title)
 
+for key,values in sorted(goals_dict.items(), reverse=True):
+    for v in values:
+        print("{}".format(v))
